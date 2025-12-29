3d405c4e2b7b6546997bfc62b2b48a9e07e3d030
aashish <ritikkamboj4314@gmail.com>
2025-12-29 21:02:58 +0530

    initial node server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+const app =express() 
+
+app.get("/home", (req, res)=>{
+    return res.json({
+        message : "OK"
+    })
+})
+
+app.listen(3000 , ()=>{
+    console.log("server is running ")
+})
